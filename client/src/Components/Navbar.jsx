b163a163352410b18a7da59c470eb3f31336b7c6
David Tomas <105463065+DadiGang-1@users.noreply.github.com>
2025-05-13 16:23:22 +0200

    Create Navbar.jsx

---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom";
+
+export default function Narbar() {
+    return (
+        <div>
+            <nav className="">
+                <NavLink to="/">
+                </NavLink>
+                
+                <NavLink className="">
+
+                </NavLink>
+            </nav>
+        </div>
+    )
+}   
